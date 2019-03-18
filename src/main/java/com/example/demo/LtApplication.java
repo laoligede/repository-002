685d99b15d6976bedbdd1fba356bc39a3467441d
tong.li01 <tong.li01@weimob.com>
2019-03-18 17:23:34 +0800

    logs

--- src/main/java/com/example/demo/LtApplication.java
+++ src/main/java/com/example/demo/LtApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LtApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LtApplication.class, args);
 	}
